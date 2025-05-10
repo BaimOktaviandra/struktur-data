93463a04752e9d23740692c01f3d5b71164d62b2
Baim Oktaviandra <baim.oktaviandra@gmail.com>
2025-05-10 23:40:13 +0700

    perkalian

---
+++ pert-1/cpp/python/latihan2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main () {
+    int a = 5;
+
+    if (a % 2 == 0) {
+        cout << a << " adalah bilangan genap" << endl;
+    } else {
+        cout << a << " adalah bilangan ganjil" << endl;
+    }
+    return 0;
+}
---
+++ pert-1/cpp/python/perkalian.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int hasil = a * b;
+    cout << "Hasil perkalian: " << hasil << endl;
+    return 0;
+}
